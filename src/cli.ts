93dc11a2cd077656148cf38e133a8e1bd93eb3cd
Bbanez <banezprod@gmail.com>
2020-08-20 14:10:52 +0200

    Initial development

--- src/cli.ts
+++ src/cli.ts
@@ -1,4 +1,7 @@
 import * as childProcess from 'child_process';
+import * as path from 'path';
+import * as fs from 'fs';
+import * as util from 'util';
 import { ArgParser } from './util';
 
 const exec = async (
@@ -28,15 +31,107 @@ const exec = async (
 export async function cli(args: string[]) {
   const options = ArgParser.parse(args);
   if (options.dev) {
-    exec('bcms-backend', (type, chunk) => {
-      process[type].write(chunk);
-    }).catch((error) => {
-      throw error;
-    });
-    exec('bcms-ui serve', (type, chunk) => {
-      process[type].write(chunk);
-    }).catch((error) => {
-      throw error;
-    });
+    if (options.backend) {
+      devBackend();
+    } else if (options.ui) {
+      devUi();
+    } else {
+      devBackend();
+      devUi();
+    }
   }
+}
+
+async function devBackend() {
+  exec('bcms-backend', (type, chunk) => {
+    process[type].write(chunk);
+  }).catch((error) => {
+    throw error;
+  });
+}
+
+async function devUi() {
+  let router = (
+    await util.promisify(fs.readFile)(
+      path.join(
+        __dirname,
+        '..',
+        'node_modules',
+        '@becomes',
+        'cms-ui',
+        'src',
+        'router',
+        'index.js',
+      ),
+    )
+  ).toString();
+  let mainJS = (
+    await util.promisify(fs.readFile)(
+      path.join(
+        __dirname,
+        '..',
+        'node_modules',
+        '@becomes',
+        'cms-ui',
+        'src',
+        'main.js',
+      ),
+    )
+  ).toString();
+  if (await util.promisify(fs.exists)(path.join(process.cwd(), 'ui'))) {
+    mainJS = `import bundlerRouter from './router/index.temp.js';\n` + mainJS;
+    router =
+      `import CustomPortal from '${path.join(
+        process.cwd(),
+        'ui',
+        'App.vue',
+      )}';\n` +
+      router.replace(
+        '// %CustomPortal%',
+        `
+        {
+          path: "/custom-portal",
+          name: "CustomPortal",
+          component: CustomPortal,
+          props: {
+            msg: 'This is test!',
+          },
+        },
+        `,
+      );
+    await util.promisify(fs.writeFile)(
+      path.join(
+        __dirname,
+        '..',
+        'node_modules',
+        '@becomes',
+        'cms-ui',
+        'src',
+        'router',
+        'index.temp.js',
+      ),
+      router,
+    );
+  }
+  await util.promisify(fs.writeFile)(
+    path.join(
+      __dirname,
+      '..',
+      'node_modules',
+      '@becomes',
+      'cms-ui',
+      'src',
+      'main.temp.js',
+    ),
+    mainJS,
+  );
+
+  process.chdir(
+    path.join(__dirname, '..', 'node_modules', '@becomes', 'cms-ui'),
+  );
+  exec('bcms-ui serve src/main.temp.js', (type, chunk) => {
+    process[type].write(chunk);
+  }).catch((error) => {
+    throw error;
+  });
 }
